ec5a9aa136b187c7fe5dca3703decd044bec5a84
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-18 18:22:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,9 @@
 def key_for_min_value(name_hash)
+  min_key = nil
+  min_value = nil
+  
   name_hash.select do |key_, value_|
-    if value_ == name_hash.values.min
+    if value_ < min_value
       return key_
 end
 end
